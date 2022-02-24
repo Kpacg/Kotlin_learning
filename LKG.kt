44f02d4c07da4444024d14781fa9bbc22bd0a1d6
Kpacg <drakon40000@gmail.com>
2022-02-25 00:23:56 +0300

    2 step

--- LKG.kt
+++ LKG.kt
@@ -1,3 +1,45 @@
-fun main(){
-    println("Привет, Юля!")
+fun readNaturalNumber():Int{
+    var NaturalNumber = 0
+    while(NaturalNumber < 1)
+    {
+        try {
+            NaturalNumber = readLine()!!.toInt()
+            if(NaturalNumber < 0)
+                throw Exception("Кто-то слишком умный!")
+        }
+        catch (e:Exception)
+        {
+            println("Кажется, ты ошиблась, попробуй ещё раз!")
+        }
+    }
+    return NaturalNumber
 }
+
+private fun LKG() {
+    val a = 567
+    val b = 91223
+    var x: Int
+
+    println("Привет, Юля! Сколько случайных (или не очень) чисел ты хотела бы получить?")
+    val amountOfNumbers: Int = readNaturalNumber()
+
+    println(
+        "Отлично, осталось выбрать начальную точку! Я бы хотел, чтобы это было целое положительное число. " +
+                "Не держи зла :3"
+    )
+    x = readNaturalNumber()
+
+    println("Поехали!")
+
+    for (i in 1..amountOfNumbers) {
+        x = a * x + b
+        println("$x")
+    }
+
+    println("Всё!")
+}
+
+fun main(){
+    LKG()
+}
+
